44dace76b75fa71914c60b4f7272b28a971816f6
aktersnurra <aktersnurra@dyn1181-247.wlan.ic.ac.uk>
2017-02-03 17:54:22 +0000

    color. etc

---
+++ src/color.py
@@ -0,0 +1,19 @@
+class Color(object):
+
+    def __int__(self,red, green, blue, clear):
+        self.red = red
+        self.green = green
+        self.blue = blue
+        self.clear = clear
+
+    def get_red(self):
+        return self.red
+
+    def get_green(self):
+        return self.green
+
+    def get_blue(self):
+        return self.blue
+
+    def get_clear(self):
+        return self.clear
