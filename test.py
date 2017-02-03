5392d098092f696b6bf80c6e8d9e2b9f07982247
Fabian Duffhauss <fabian.duffhauss@outlook.com>
2017-02-03 16:46:07 +0000

    wifi + RGB added

--- test.py
+++ test.py
@@ -20,8 +20,8 @@ client.publish('esys/SpuxnetInc', bytes('hello world!', 'utf-8'))
 client.disconnect()
 sta_if.disconnect()
 
-i2c = I2C(scl=Pin(4), sda=Pin(5), freq=100000)
+#i2c = I2C(scl=Pin(4), sda=Pin(5), freq=100000)
 # address of RGB color sensor is 0x29
-i2c.writeto(0x29, '0')
-print(i2c.readfrom(0x29, 3))
+#i2c.writeto(0x29, '0')
+#print(i2c.readfrom(0x29, 3))
 
