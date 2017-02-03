09f5096dfa771fa6a68d37fa9494bee947c53b0e
Lukas Hann <lukas.hann95@gmail.com>
2017-02-03 14:37:02 +0000

    now retries connection every 5 seconds, started i2c stuff

--- test.py
+++ test.py
@@ -1,16 +1,17 @@
 import machine
+from machine import Pin, I2C
 import time
 import network
 from umqtt.simple import MQTTClient
 
 ap_if = network.WLAN(network.AP_IF)
 ap_if.active(False)
+
 sta_if = network.WLAN(network.STA_IF)
-
-
-if sta_if.isconnected() == False:
-	sta_if.active(True)
+sta_if.active(True)
+while sta_if.isconnected() == False:
 	sta_if.connect('EEERover', 'exhibition')
+	time.sleep(5)
 	
 client = MQTTClient(machine.unique_id(),'192.168.0.10')
 client.connect()
@@ -19,5 +20,8 @@ client.publish('esys/SpuxnetInc', bytes('hello world!', 'utf-8'))
 client.disconnect()
 sta_if.disconnect()
 
-
+i2c = I2C(scl=Pin(4), sda=Pin(5), freq=100000)
+# address of RGB color sensor is 0x29
+i2c.writeto(0x29, '0')
+print(i2c.readfrom(0x29, 3))
 
