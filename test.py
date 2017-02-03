0e4588f45ec9dccfee7a7137543adc87ff03d215
Lukas Hann <lukas.hann95@gmail.com>
2017-02-03 14:58:04 +0000

    added driver for tcs34725

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 import machine
-from machine import Pin, I2C
 import time
 import network
 from umqtt.simple import MQTTClient
@@ -20,8 +19,9 @@ client.publish('esys/SpuxnetInc', bytes('hello world!', 'utf-8'))
 client.disconnect()
 sta_if.disconnect()
 
-i2c = I2C(scl=Pin(4), sda=Pin(5), freq=100000)
+i2c = machine.I2C(scl=machine.Pin(5), sda=machine.Pin(4), freq=100000)
 # address of RGB color sensor is 0x29
-i2c.writeto(0x29, '0')
-print(i2c.readfrom(0x29, 3))
+#i2c.writeto(0x29, '0')
+print(i2c.readfrom(0x29, 10))
+#print(i2c.scan())
 
