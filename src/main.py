47f3658f771a5559217d1d47317f8649abf48e89
Lukas Hann <lukas.hann95@gmail.com>
2017-02-09 11:59:44 +0000

    reads RGB colors 10 times and ony flashes LED when measuring

--- src/main.py
+++ src/main.py
@@ -1,28 +1,42 @@
 import time
 import tcs34725
 import Colour
+import machine
 from closestColour import closestColour
-from machine import Pin, I2C
 from WifiBroker import WifiBroker
 
 
 def main():
+
+	# set up I2C for interfacing with the RGB sensor
+	i2c = machine.I2C(scl=machine.Pin(5), sda=machine.Pin(4), freq=100000)
+	# set up the Sensor
+	sensor = tcs34725.TCS34725(i2c)
+	# configure pin 15 to turn the LED built into the sensor on when measuring and off otherwise
+	sensorLED = machine.Pin(15, machine.Pin.OUT)
+	# set the sensor LED to low to turn off the LED
+	sensorLED.low()
+
+	# set up the wifiBroker and connect to the broker
 	wifiBroker = WifiBroker()
 	wifiBroker.connect_to_broker()
-
-	i2c = I2C(scl=Pin(5), sda=Pin(4), freq=100000)
-	sensor = tcs34725.TCS34725(i2c)
 
 	for i in range(0, 10):
 		RedList = []
 		GreenList = []
 		BlueList = []
 
+		sensorLED.high()
+		time.sleep(0.9)
+
 		for j in range(0, 20):
 			(red, green, blue, clear) = sensor.read(True)
+			sensorLED.low()
 			RedList.append(red)
 			BlueList.append(blue)
 			GreenList.append(green)
+
+		time.sleep(0.1)
 
 		averageRed = sum(RedList)/float(len(RedList))
 		averageGreen = sum(GreenList)/float(len(GreenList))
@@ -32,9 +46,8 @@ def main():
 		match = closestColour(readColour)
 		wifiBroker.publish_msg(match)
 
-		time.sleep(1)
-
 
 	wifiBroker.disconnect()
 
 main()
+
